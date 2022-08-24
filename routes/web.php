32af2ec30c58788f37a1093e3da86775c9bde587
BominsoeMM <bominsoe@Bos-MacBook-Air.local>
2022-08-24 18:56:29 +0630

    error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+//use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,11 +13,11 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', [PostController::class,'index'])->name('post.index');
-Route::get('/create', [PostController::class,'create'])->name('post.create');
-Route::post('/create', [PostController::class,'store'])->name('post.store');
-Route::get('/show/{id}', [PostController::class,'show'])->name('post.show');
-Route::delete('/destroy/{id}', [PostController::class,'destroy'])->name('post.destroy');
-Route::get('/edit/{id}', [PostController::class,'edit'])->name('post.edit');
-Route::put('/update/{id}', [PostController::class,'update'])->name('post.update');
+//Route::get('/', [PostController::class,'index'])->name('post.index');
+//Route::get('/create', [PostController::class,'create'])->name('post.create');
+//Route::post('/create', [PostController::class,'store'])->name('post.store');
+//Route::get('/show/{id}', [PostController::class,'show'])->name('post.show');
+//Route::delete('/destroy/{id}', [PostController::class,'destroy'])->name('post.destroy');
+//Route::get('/edit/{id}', [PostController::class,'edit'])->name('post.edit');
+//Route::put('/update/{id}', [PostController::class,'update'])->name('post.update');
 
